164a62aa4e52ad14ca3d910c978b9ffa37b9f287
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2023-07-14 13:04:14 +0900

    準備

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+const config = {
+  siteUrl: 'https://futabastep.net',
+  copyright: {
+    year: 2023,
+    author: 'Nectarition Blossom'
+  },
+  event: {
+    name: 'ふたばすてっぷ',
+    date: '2024-06-02',
+    place: '福生・ポプルスガレージ',
+    content: '双葉湊音オンリー即売会',
+    twitterId: 'futabastep'
+  }
+}
+
+export default config
